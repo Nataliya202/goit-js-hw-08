1a2cffae8a6006ef95051102833f6594a9a41065
Nataliya202 <natamarty2017@gmail.com>
2023-06-22 11:00:27 +0300

    update second commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const VCT_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (event) {
+    localStorage.setItem(VCT_KEY, event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(VCT_KEY)) || 0);
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle';
+import '../css/common.css';
+import '../css/03-feedback.css';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {
+    email: '',
+    message: '',
+};
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    input: document.querySelector('input')
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateTextarea();
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem('STORAGE_KEY');
+    console.log(formData);
+ }
+
+function onTextareaInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    const stringifiedData = JSON.stringify(formData);
+    localStorage.setItem('STORAGE_KEY', stringifiedData);
+}
+ 
+function populateTextarea() {
+    const savedMessage = localStorage.getItem('STORAGE_KEY');
+    if (savedMessage === null) {
+        return;
+    }
+    refs.textarea.value = savedMessage.message || '';
+    refs.input.value = savedMessage.email || '';
+    formData.email = savedMessage.email || '';
+    formData.message = savedMessage.message || '';
+    
+ }
+
+
+
+
 
